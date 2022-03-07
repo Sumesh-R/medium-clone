100ab26bf59f9dfbba3f672e97f6045fd3727f3a
Sumesh-R <rsumesh2020@gmail.com>
2022-03-07 20:15:01 +0530

    added rickroll to 404

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+function E404() {
+  return (
+    <div className="flex items-stretch">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/QNXvE1BZu8g"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; encrypted-media;"
+      ></iframe>
+    </div>
+  )
+}
+
+export default E404
